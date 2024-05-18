23c3b5660e93799d2509fd0cf911db9f0f8fa043
dineli <dinelikarunathilaka@gmail.com>
2024-05-18 18:20:22 +0800

    adding project

---
+++ java-maven-junit-helloworld-master/src/test/java/com/example/javamavenjunithelloworld/ExceptionAssertionUnitTest.java
@@ -0,0 +1,28 @@
+package com.example.javamavenjunithelloworld;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ExceptionAssertionUnitTest {
+	@Test
+	void whenExceptionThrown_thenAssertionSucceeds() {
+		Exception exception = assertThrows(NumberFormatException.class, () -> Integer.parseInt("1a"));
+
+		String expectedMessage = "For input string";
+		String actualMessage = exception.getMessage();
+
+		assertTrue(actualMessage.contains(expectedMessage));
+	}
+
+	@Test
+	void whenDerivedExceptionThrown_thenAssertionSucceeds() {
+		Exception exception = assertThrows(RuntimeException.class, () -> Integer.parseInt("1a"));
+
+		String expectedMessage = "For input string";
+		String actualMessage = exception.getMessage();
+
+		assertTrue(actualMessage.contains(expectedMessage));
+	}
+}
